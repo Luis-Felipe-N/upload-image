be2a26b0efb62321eaa2136239519c489e903a35
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-09-07 21:10:16 -0300

    finish page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Box } from '@chakra-ui/react';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
@@ -9,6 +9,14 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 export default function Home(): JSX.Element {
+  const fetchImages = async ({pageParam = 1}) => {
+    const res = await api.get('api/images')
+    const { data } = res.data;
+    return {
+      data,
+    };
+  }
+
   const {
     data,
     isLoading,
@@ -18,18 +26,35 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    fetchImages,
+    {
+      getNextPageParam: l => l.data.pageParams
+    }
   );
+
+  useEffect(() => {console.log(data )}, [data])
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
   }, [data]);
 
+  const handleFetchNextPage = () => {
+    fetchNextPage: fetchImages
+  }
+
   // TODO RENDER LOADING SCREEN
+  if (isLoading){
+    return (
+      <Loading />
+    )
+  }
 
   // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return (
+      <Error />
+    )
+  }
 
   return (
     <>
@@ -38,6 +63,15 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button 
+            isLoading={isFetchingNextPage}
+            loadingText="Carregando..."
+            onClick={() => fetchNextPage()}>
+              Ver mais
+          </Button>
+        )}
       </Box>
     </>
   );
